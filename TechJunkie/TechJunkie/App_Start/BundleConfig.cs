1681c4b9937fa9bd23aa02d451f370828131c5fd
thecookiejar <nil>
2016-09-15 10:27:29 +0800

    update

---
+++ TechJunkie/TechJunkie/App_Start/BundleConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace TechJunkie
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Vendor/Components/jquery/dist/jquery.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                        "~/Vendor/Components/bootstrap/dist/js/bootstrap.min.js"));
+
+            bundles.Add(new StyleBundle("~/bundles/bootstrap_css").Include(
+                      "~/Vendor/Components/bootstrap/dist/css/bootstrap.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/Styles.css"));
+
+        }
+    }
+}
--- TechJunkie/TechJunkie/Global.asax.cs
+++ TechJunkie/TechJunkie/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Optimization;
 
 namespace TechJunkie
 {
@@ -13,6 +14,7 @@ namespace TechJunkie
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
